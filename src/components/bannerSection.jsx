f8930a67e88a03ff6448fa98a7f210909c2fda93
Shahriar-Samir <shabusiness035@gmail.com>
2024-04-28 14:27:01 +0600

    designed banner slides and added simple type writer component

--- src/components/bannerSection.jsx
+++ src/components/bannerSection.jsx
@@ -3,13 +3,15 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Typewriter } from 'react-simple-typewriter'
+
 const BannerSection = () => {
     return (
-        <div className='mx-auto w-11/12 max-w-[1200px]'>
+        <div className='mx-auto w-full max-w-[1200px]'>
         <Swiper
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 4000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -19,18 +21,21 @@ const BannerSection = () => {
         className="mySwiper"
       >
         <SwiperSlide className="flex justify-center items-center bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/banner1.jpg')] bg-no-repeat bg-cover bg-center h-full">
-            <div className='h-[500px] flex justify-start items-center'>
-                <h1 className='text-white text-8xl font-bold ms-5'>Discover Natural Elegance</h1>
+            <div className='h-[550px] flex flex-col justify-center items-start text-white gap-5'>
+                <h1 className='text-7xl font-bold ms-5'><Typewriter loop={1} cursor cursorStyle='_'  words={['Discover Natural Elegance']}/></h1>
+                <p className='w-11/12 max-w-[700px]  ms-5'>Immerse yourself in the allure of eco-friendly jute crafts! Our collection showcases a range of handcrafted jute home decor, from exquisite wall hangings to charming coasters. Embrace the earthy tones and rustic textures of jute as you adorn your living space with sustainable elegance. Each piece is thoughtfully designed to bring a touch of nature's beauty into your home.</p>
             </div>
         </SwiperSlide>
         <SwiperSlide className="flex justify-center items-center bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/banner2.jpg')] bg-no-repeat bg-cover bg-center h-full">
-        <div className='h-[500px]'>
-            
+        <div className='h-[550px] flex flex-col justify-center items-start text-white gap-5'>
+        <h1 className='text-7xl font-bold ms-5'><Typewriter deleteSpeed={0} loop={2} cursor cursorStyle='_'  words={["Crafted with Nature's Beauty"]}/></h1>
+                <p className='w-11/12 max-w-[700px]  ms-5'>Indulge in the warmth of artisanal wooden craftsmanship! Dive into our curated collection of handmade wooden home decor and kitchenware, meticulously crafted to elevate your living spaces. From graceful serving trays to functional utensils, each piece celebrates the inherent beauty of natural wood. Imbue your home with the timeless charm of handcrafted wooden creations.</p>
             </div>
         </SwiperSlide>
         <SwiperSlide className="flex justify-center items-center bg-[linear-gradient(to_top,rgba(0,0,0,0.4),rgba(0,0,0,0)),linear-gradient(to_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0)),url('/images/banner3.jpg')] bg-no-repeat bg-cover bg-center h-full">
-        <div className='h-[500px]'>
-            
+        <div className='h-[550px] flex flex-col justify-center items-start text-white gap-5'>
+        <h1 className='text-7xl font-bold ms-5'><Typewriter loop={3} typeSpeed={100} cursor cursorStyle='_'  words={["Make it Yours!"]}/></h1>
+                <p className='w-11/12 max-w-[700px]  ms-5'>Transform ordinary objects into cherished treasures with our personalized customization options! Explore the art of personalization and infuse your favorite jute and wooden crafts with a touch of individuality. From engraved initials on wooden photo frames to embroidered monograms on jute tote bags, unleash your creativity and make every piece uniquely yours.</p>
             </div>
         </SwiperSlide>
       </Swiper>
