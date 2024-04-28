d2cc4c9950fd35cdf99d8728d7c6f6bf248e13ba
Shahriar-Samir <shabusiness035@gmail.com>
2024-04-28 23:14:15 +0600

    craft designed item details page

--- src/components/craftItemDetails.jsx
+++ src/components/craftItemDetails.jsx
@@ -1,30 +1,40 @@
 import { useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const CraftItemDetails = () => {
+    const navigate = useNavigate()
     const data = useLoaderData()
+
 
     useEffect(()=>{
         document.querySelector('html').setAttribute('data-theme', 'light')
     },[])
     return (
-        <div className='mt-5 w-10/12 max-w-[1000px] mx-auto'>
-            <h1 className='text-center text-3xl font-bold'>Craft Details</h1>
-            <div className='mt-16 flex gap-10'>
-            <img className='w-1/2 object-cover' src={`${data.image}`}/>
-            <div className='flex flex-col'>
-            <p>Name : {data.itemName}</p>
-            <p>Subcategory : {data.subcategoryName}</p>
-            <p>Price : {data.price}</p>
-            <p>Rating : {data.rating}</p>
-            <p>Customization : {data.customization}</p>
-            <p>Processing Time : {data.processingTime}</p>
-            <p>Stock Status : {data.stockStatus}</p>
+        <div className='mt-10 w-10/12 max-w-[1000px] mx-auto'>
+            <div className=''>
+                <button className='btn bg-[#CF883C] text-white' onClick={()=>  navigate(-1)}><IoMdArrowRoundBack className='text-xl'/> Go Back</button>
             </div>
+            <h1 className='text-center text-4xl font-bold'>Craft Details</h1>
+            <div className='mt-10 flex flex-col md:flex-row items-center md:items-start gap-10'>
+            <img className='w-full md:w-1/2 object-cover' src={`${data.image}`}/>
+            <div className='flex flex-col gap-5'>
+            <p className='text-sm font-bold'>Subcategory: {data.subcategoryName}</p>
+            <p className='text-2xl font-bold'> {data.itemName}</p>
+            <p className='text-xl text-[#CF883C] font-bold'>${data.price}</p>
+            <p className='text-xl text-[#FB923C] flex items-center gap-4'><span className=' font-bold'>Rating:</span> 
+            <div className='rating'>
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
             </div>
-            <div className='mt-5'>
-                <h1 className='text-2xl font-bold'>Descriptions</h1>
+            {data.rating} out of 5</p>
+            <div className='text-xl'>
+ 
                 <p>{data.shortDescription}</p>
+            </div>
+            <p className='text-xl'><span className=' font-bold'>Customization</span>: {data.customization}</p>
+            <p className='text-xl'><span className=' font-bold'>Processing Time</span>: {data.processingTime}</p>
+            <p className='text-xl flex gap-5 items-center'><span className=' font-bold'>Stock Status:</span> <span className={`p-2  text-white rounded-md ${data.stockStatus === 'In stock'? 'bg-green-500':'bg-gray-300' }`}>{data.stockStatus}</span></p>
+            </div>
             </div>
         </div>
     );
