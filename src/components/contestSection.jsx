e9c9495a1fa8158980dce75f48a1743333538204
Shahriar-Samir <shabusiness035@gmail.com>
2024-04-28 17:35:06 +0600

    designed contest section

--- src/components/contestSection.jsx
+++ src/components/contestSection.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const ContestSection = () => {
     return (
-        <div className='h-[450px] flex items-center px-10'>
-            <div>
+        <div className='h-[450px] flex items-center mx-auto w-10/12 mt-24 px-10'>
+            <div className='w-1/2'>
             <h1 className='text-6xl font-bold'>Craft Masters Challenge</h1>
-            <p>Embark on the Craft Masters Challenge and put your crafting skills to the test! Create stunning jute and wooden masterpieces that showcase your talent and creativity. Compete against fellow crafters, earn recognition for your craftsmanship, and win fabulous prizes. Unleash your imagination and join the ultimate crafting showdown today!</p>
+            <p className='mt-5'>Embark on the Craft Masters Challenge and put your crafting skills to the test! Create stunning jute and wooden masterpieces that showcase your talent and creativity. Compete against fellow crafters, earn recognition for your craftsmanship, and win fabulous prizes. Unleash your imagination and join the ultimate crafting showdown today!</p>
+            <button className='btn bg-[#CF883C] hover:bg-[#a56c2f] mt-5 text-white'>Participate</button>
             </div>
-            <div className='flex'>
-            <img src='/images/contest.jpg' className=''/>
-            <img src='/images/contest.jpg' className=''/>
-            </div>
+ 
+            <img src='/images/contest.jpg' className='w-1/2'/>
+
         </div>
     );
 };
